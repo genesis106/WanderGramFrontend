330b576192c9536e5851a7353b747fb23fbb325f
Payal Sahu <144543864+genesis106@users.noreply.github.com>
2025-04-26 03:17:17 +0530

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,30 +4,27 @@ import Title from '../components/title';
 import NewsBox from '../components/newsBox';
 
 const Home = () => {
-  const [filter, setFilter] = useState('all'); // Default filter
+  const [filter, setFilter] = useState('all');
 
-  // Handle the filter change from Sidebar
   const handleFilterChange = (newFilter) => {
     setFilter(newFilter);
   };
 
   return (
-    <div className="flex flex-col lg:flex-row h-screen">
-      {/* Sidebar */}
-      <div className="w-full lg:w-1/4 bg-gray-100 p-4">
+<div className="flex flex-col lg:flex-row h-screen bg-white" style={{ padding: 0, margin: 0 }}>      <div className="w-full lg:w-1/4 bg-gray-100 p-4 lg:h-screen lg:overflow-y-auto">
         <Sidebar onFilterChange={handleFilterChange} />
       </div>
 
       {/* Main Content */}
-      <div className="w-full lg:w-3/4 flex flex-col">
+      <div className="w-full lg:w-3/4 flex flex-col h-screen" style={{ padding: 0 }}>
         {/* Title */}
-        <div className="bg-[#dcd8f3]">
+        <div className="bg-[#dcd8f3] sticky top-0 z-10" style={{ padding: 0 }}>
           <Title />
         </div>
 
         {/* NewsBox */}
-        <div className="flex-grow p-4 overflow-y-auto">
-          <NewsBox filter={filter} /> {/* Passing filter prop */}
+        <div className="flex-grow overflow-y-auto" style={{ padding: 0, margin: 0 }}>
+          <NewsBox filter={filter} />
         </div>
       </div>
     </div>
