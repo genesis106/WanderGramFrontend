ba8f5dcf2c19d87d1ec9e52bfbe8d9475ecc800f
Payal Sahu <144543864+genesis106@users.noreply.github.com>
2025-04-26 03:13:05 +0530

    Update newsBox.jsx

--- src/components/newsBox.jsx
+++ src/components/newsBox.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import NewsCards from './newsCards';
 
-const NewsBox = ({ filter }) => {  // Accept filter prop passed from Home.jsx
+const NewsBox = ({ filter }) => {
   return (
-    <div className="container mx-auto p-4 flex-1">
+    <div className="flex-1 p-4 sm:p-6 lg:p-8" style={{ margin: 0 }}>
       <h1 className="text-2xl font-bold mb-4">Latest News</h1>
-      <NewsCards filter={filter} /> {/* Pass the filter to NewsCards */}
+      <NewsCards filter={filter} />
     </div>
   );
 };
